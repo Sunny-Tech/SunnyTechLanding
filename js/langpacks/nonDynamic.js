37300dafd27b458cd115bf17a87ba140fb9d2356
Baptiste HAUDEGAND <baptiste.haudegand@teads.tv>
2017-10-13 15:55:54 +0200

    Add translations

---
+++ js/langpacks/nonDynamic.js
@@ -0,0 +1,17 @@
+Lang.prototype.pack.en = {
+	"token": {
+        "value-proposition":"test",
+        "more-info":"learn more",
+        "contact":"Contact",
+        "conf":"Conference",
+        "conf-text":"TODO",
+        "workshop":"Workshop",
+        "workshop-text":"TODO",
+        "party":"Party",
+        "party-text":"TODO",
+        "cfp":"CFP",
+        "cfp-text":"Coming soon...",
+        "contact-us":"Contact us",
+        "news":"Keep up to date !"
+	}
+};
